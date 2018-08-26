09dbb7a7ee41d3159d318a6576359669a65c3e82
Y-Wakuta <prsprivatestock@i.softbank.jp>
2018-08-27 00:44:03 +0900

    ファイル作成

---
+++ vscode_install_ex.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+code --install-extension abusaidm.html-snippets
+code --install-extension bung87.rails
+code --install-extension bung87.vscode-gemfile
+code --install-extension christian-kohler.npm-intellisense
+code --install-extension eamodio.gitlens
+code --install-extension eg2.vscode-npm-script
+code --install-extension goessner.mdmath
+code --install-extension MS-CEINTL.vscode-language-pack-ja
+code --install-extension ms-vscode.vs-keybindings
+code --install-extension oderwat.indent-rainbow
+code --install-extension PeterJausovec.vscode-docker
+code --install-extension rebornix.ruby
+code --install-extension robertohuertasm.vscode-icons
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension tomoki1207.pdf
+code --install-extension vscodevim.vim
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension Zignd.html-css-class-completion
